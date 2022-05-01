350b3777855112ff58d045acf8d6069fe20e6207
haiworldss <103318941+haiworldss@users.noreply.github.com>
2022-05-01 15:45:48 +0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,7 +66,8 @@ async def start(bot, cmd):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🔎 Cari Anime', switch_inline_query_current_chat='')
+                        InlineKeyboardButton('🔎 Cari Lagi', switch_inline_query_current_chat=query),
+                        InlineKeyboardButton('🔗 Bagikan Bot', switch_inline_query=''),
                     ]
                     ]
                 await bot.send_cached_media(
