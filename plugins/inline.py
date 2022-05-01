48d1b8cbba24a3df225ab90583baa27bfba194b8
haiworldss <103318941+haiworldss@users.noreply.github.com>
2022-05-01 15:44:18 +0700

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -90,8 +90,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('🔎 Cari Lagi', switch_inline_query_current_chat=query),
+            InlineKeyboardButton('🔗 Bagikan Bot', switch_inline_query='')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
